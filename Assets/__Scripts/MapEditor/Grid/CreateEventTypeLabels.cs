9594d8d376a4500eee21e310ff1f794a240f40e0
dcljr <dcljr@me.com>
2020-09-21 16:02:46 -0700

    added mirror button to UI and updated event type font

--- Assets/__Scripts/MapEditor/Grid/CreateEventTypeLabels.cs
+++ Assets/__Scripts/MapEditor/Grid/CreateEventTypeLabels.cs
@@ -2,13 +2,13 @@
 using TMPro;
 
 public class CreateEventTypeLabels : MonoBehaviour {
-
-    public TMP_FontAsset AvailableAsset;
-    public TMP_FontAsset UtilityAsset;
-    public TMP_FontAsset RedAsset;
+    
+    public Color AvailableAssetColor;
+    public Color UtilityAssetColor;
+    public Color RedAssetColor;
     public GameObject LayerInstantiate;
     public Transform[] EventGrid;
-    [SerializeField] private DarkThemeSO darkTheme;
+    //[SerializeField] private DarkThemeSO darkTheme;
 
     private LightsManager[] LightingManagers;
 
@@ -36,19 +36,19 @@ public class CreateEventTypeLabels : MonoBehaviour {
                 TextMeshProUGUI textMesh = instantiate.GetComponentInChildren<TextMeshProUGUI>();
                 if (isPropagation)
                 {
-                    textMesh.font = UtilityAsset;
+                    textMesh.color = UtilityAssetColor;
                     if (i == 0)
                     {
                         textMesh.text = "All Lights";
-                        textMesh.font = RedAsset;
+                        textMesh.color = RedAssetColor;
                     }
                     else
                     {
                         textMesh.text = $"{LightingManagers[eventType].name} ID {i}";
                         if (i % 2 == 0)
-                            textMesh.font = UtilityAsset;
+                            textMesh.color = UtilityAssetColor;
                         else
-                            textMesh.font = AvailableAsset;
+                            textMesh.color = AvailableAssetColor;
                     }
                 }
                 else
@@ -56,39 +56,39 @@ public class CreateEventTypeLabels : MonoBehaviour {
                     switch (i)
                     {
                         case MapEvent.EVENT_TYPE_RINGS_ROTATE:
-                            textMesh.font = UtilityAsset;
+                            textMesh.color = UtilityAssetColor;
                             textMesh.text = "Ring Rotation";
                             break;
                         case MapEvent.EVENT_TYPE_RINGS_ZOOM:
-                            textMesh.font = UtilityAsset;
+                            textMesh.color = UtilityAssetColor;
                             textMesh.text = "Ring Zoom";
                             break;
                         case MapEvent.EVENT_TYPE_LEFT_LASERS_SPEED:
+                            textMesh.color = UtilityAssetColor;
                             textMesh.text = "Left Laser Speed";
-                            textMesh.font = UtilityAsset;
                             break;
                         case MapEvent.EVENT_TYPE_RIGHT_LASERS_SPEED:
+                            textMesh.color = UtilityAssetColor;
                             textMesh.text = "Right Laser Speed";
-                            textMesh.font = UtilityAsset;
                             break;
                         case MapEvent.EVENT_TYPE_EARLY_ROTATION:
+                            textMesh.color = UtilityAssetColor;
                             textMesh.text = "Rotation (Include)";
-                            textMesh.font = UtilityAsset;
                             break;
                         case MapEvent.EVENT_TYPE_LATE_ROTATION:
+                            textMesh.color = UtilityAssetColor;
                             textMesh.text = "Rotation (Exclude)";
-                            textMesh.font = UtilityAsset;
                             break;
                         case MapEvent.EVENT_TYPE_BOOST_LIGHTS:
+                            textMesh.color = UtilityAssetColor;
                             textMesh.text = "Boost Lights";
-                            textMesh.font = UtilityAsset;
                             break;
                         default:
                             if (LightingManagers.Length > i)
                             {
                                 LightsManager customLight = LightingManagers[i];
                                 textMesh.text = customLight?.name;
-                                textMesh.font = AvailableAsset;
+                                textMesh.color = AvailableAssetColor;
                             }
                             else
                             {
@@ -96,10 +96,11 @@ public class CreateEventTypeLabels : MonoBehaviour {
                             }
                             break;
                     }
+                    /*
                     if (Settings.Instance.DarkTheme)
                     {
                         textMesh.font = darkTheme.TekoReplacement;
-                    }
+                    }*/
                 }
             }
             catch { }
