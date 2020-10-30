2af02f5e245e1871e28abb9cb6dffa71a4d613da
dcljr <dcljr@me.com>
2020-10-29 17:59:21 -0700

    Added ExtensionButtons

---
+++ Assets/__Scripts/MapEditor/UI/Extensions/ExtensionButton.cs
@@ -0,0 +1,36 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class ExtensionButton : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private Tooltip tooltip;
+    [SerializeField] private Image icon;
+    [SerializeField] private Button button;
+
+    public string Text
+    {
+        get => text.text;
+        set => text.text = value;
+    }
+
+    public string Tooltip
+    {
+        get => tooltip.tooltipOverride;
+        set => tooltip.tooltipOverride = value;
+    }
+
+    public Sprite Icon
+    {
+        get => icon.sprite;
+        set => icon.sprite = value;
+    }
+
+    public void AddOnClick(UnityAction onClick)
+    {
+        button.onClick.RemoveAllListeners();
+        button.onClick.AddListener(onClick);
+    }
+}
---
+++ Assets/__Scripts/MapEditor/UI/Extensions/ExtensionButtons.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ExtensionButtons : MonoBehaviour
+{
+    public static ExtensionButtons instance; //null unless in editor scene
+
+    [SerializeField] private Transform container;
+    [SerializeField] private ExtensionButton prefab;
+
+    public void AddButton(string text, string tooltip, Sprite icon, UnityAction onClick)
+    {
+        ExtensionButton extensionButton = Instantiate(prefab, container);
+        extensionButton.Text = text;
+        extensionButton.Tooltip = tooltip;
+        extensionButton.Icon = icon;
+        extensionButton.AddOnClick(onClick);
+    }
+
+    public void RemoveButton(string text)
+    {
+        ExtensionButton toDelete = container.GetComponentsInChildren<ExtensionButton>().FirstOrDefault(x => x.Text == text);
+        if (toDelete != null)
+            Destroy(toDelete.gameObject);
+    }
+
+    void Awake()
+    {
+        instance = this;
+    }
+}
